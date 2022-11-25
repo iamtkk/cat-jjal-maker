c4c32c1ffa9aede11db2c95838b5ec337f783ee8
김태관 <iamtk@neowiz.com>
2022-11-25 22:29:57 +0900

    study

--- resources/utils.js
+++ resources/utils.js
@@ -1,9 +1,12 @@
-const EMPTY_HEART = "🤍";
-const FULL_HEART = "💖";
+const EMPTY_HEART = '🤍';
+const FULL_HEART = '💖';
 
-const CAT1 = "https://cataas.com/cat/60b73094e04e18001194a309/says/react";
-const CAT2 = "https://cataas.com/cat/5e9970351b7a400011744233/says/inflearn";
-const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
+const CAT1 =
+  'https://cdn.cloudflare.steamstatic.com/steam/apps/1627720/ss_ee0d56a44bfd75006aebd4e2c84c875d68b68647.1920x1080.jpg?t=1666835591';
+const CAT2 =
+  'https://cdn.cloudflare.steamstatic.com/steam/apps/1627720/ss_3bb48e7309ec845d0a69cef05f901085a4714c53.1920x1080.jpg?t=1666835591';
+const CAT3 =
+  'https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/08/lies-of-p-game-pass-tease-snitch-580x334.jpg';
 
 const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
 
@@ -17,7 +20,7 @@ const jsonLocalStorage = {
 };
 
 const fetchCat = async (text) => {
-  const OPEN_API_DOMAIN = "https://cataas.com";
+  const OPEN_API_DOMAIN = 'https://cataas.com';
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
   const responseJson = await response.json();
   return `${OPEN_API_DOMAIN}/${responseJson.url}`;
